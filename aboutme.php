83812c6d263986ee6150d8518d08c27f025962aa
Fransisco Allagan <63401643+fransisco27@users.noreply.github.com>
2020-04-09 20:30:10 +0700

    membuat file aboutme.php

---
+++ aboutme.php
@@ -0,0 +1,11 @@
+<?php
+echo "Nama Lengkap saya : Fransisco Allagan";
+echo "<br>";
+echo "Kelas : XI RPL 2";
+echo "<br>";
+echo "Sekolah : SMK Taruna Bhakti";
+echo "<br>";
+echo "Alamat : Jl. Gotong royong Jatijajar Tapos Depok";
+echo "<br>";
+echo "Alamat Email : fransiskoallagan@gmail.com";
+?>
