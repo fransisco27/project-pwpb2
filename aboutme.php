4f9b7478c2fbcf5e379cb33ee3f82263b317b188
Fransisco Allagan <63401643+fransisco27@users.noreply.github.com>
2020-04-09 21:26:43 +0700

    hapus baris alamat rumah

--- aboutme.php
+++ aboutme.php
@@ -5,8 +5,6 @@ echo "Kelas : XI RPL 2";
 echo "<br>";
 echo " Asal Sekolah : SMK Taruna Bhakti";
 echo "<br>";
-echo " Alamat rumah : Jl. Gotong Royong Jatijajar Tapos Depok";
-echo "<br>";
 echo " No HP : 0896 5324 8454";
 echo "<br>";
 echo " Alamat Email : fransiskoallagan@gmail.com";
